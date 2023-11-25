2c9199b3a3791bccc915111ea5e86fb82e25ec51
Rammah Karpous <rammahkarpous@outlook.com>
2023-11-25 12:38:06 +0100

    create a php router

--- index.php
+++ index.php
@@ -2,14 +2,4 @@
 
 require 'functions.php';
 
-$uri = $_SERVER['REQUEST_URI'];
-
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/contact' => 'controllers/contact.php'
-];
-
-if (array_key_exists($uri, $routes)) {
-    require 'controllers/{}.php';
-}
+require 'router.php';
